f739e14074a099bceeef6d580dcc48fbc871784f
Juan B Cabral <jbc.develop@gmail.com>
2016-03-22 13:27:30 -0300

    skew choices started

--- preference_for_skew/views.py
+++ preference_for_skew/views.py
@@ -11,6 +11,9 @@ from .libs import graph
 
 class Screen1(Page):
 
+    form_model = models.Player
+    form_fields = ['skewchoice1']
+
     def vars_for_template(self):
         return {"graph": graph.render(**Constants.pie_conf)}
 
