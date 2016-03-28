faa7b9f8c59c4fcc652f8ff8ecbb35581af24875
Juan B Cabral <jbc.develop@gmail.com>
2016-03-28 01:39:30 -0300

    fix

--- preference_for_skew/models.py
+++ preference_for_skew/models.py
@@ -23,11 +23,10 @@ class Constants(BaseConstants):
     name_in_url = 'preference_for_skew'
     players_per_group = None
     num_rounds = 1
-    pie_data = [10, 40, 50]
+    pie_data = [50, 49, 1]
     pie_conf = {
         "data": pie_data,
         "labels": ["{}%".format(d) for d in pie_data],
-        "title": "Please Choose your most preferred gamble",
         "colors": ['#EE9095', '#90EE90', '#AB90EE']
     }
     skew_choices = ["A", "B", "C", "D", "E", "F"]
@@ -39,6 +38,24 @@ class Constants(BaseConstants):
         'D': [-0.2, 3.4, 3.4],
         'E': [-0.6, 4.2, 4.2],
         'F': [-1.2, 4.8, 4.8]
+    }
+
+    skewchoices2 = {
+        'A': [1., 1., 1.,],
+        'B': [0.65, 1.71, 4.07],
+        'C': [0.3, 2.41, 7.15],
+        'D': [-0.06, 3.12, 10.22],
+        'E': [-0.41, 3.82, 13.29],
+        'F': [-0.96, 4.33, 16.17]
+    }
+
+    skewchoices3 = {
+        'A': [1.0, 1.0, 1.0],
+        'B': [0.65, 1.71, 4.07],
+        'C': [0.30, 2.41, 7.15],
+        'D': [-0.06, 3.12, 10.22],
+        'E': [-0.41, 3.82, 13.29],
+        'F': [-0.96, 4.33, 16.17]
     }
 
 
@@ -53,5 +70,5 @@ class Group(BaseGroup):
 class Player(BasePlayer):
 
     skewchoice1 = models.CharField(max_length=50, choices=sorted(Constants.skewchoices1), widget=widgets.RadioSelectHorizontal())
-    skewchoice2 = models.CharField(max_length=50, choices=Constants.skew_choices, widget=widgets.RadioSelectHorizontal())
-    skewchoice3 = models.CharField(max_length=50, choices=Constants.skew_choices, widget=widgets.RadioSelectHorizontal())
+    skewchoice2 = models.CharField(max_length=50, choices=sorted(Constants.skewchoices2), widget=widgets.RadioSelectHorizontal())
+    skewchoice3 = models.CharField(max_length=50, choices=sorted(Constants.skewchoices3), widget=widgets.RadioSelectHorizontal())
--- preference_for_skew/views.py
+++ preference_for_skew/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 
+from collections import OrderedDict
+
 from otree.common import Currency as c, currency_range, safe_json
 
 from . import models
@@ -9,20 +11,81 @@ from .models import Constants
 from .libs import graph
 
 
-class Screen1(Page):
+class AbstractScreen(object):
+
+    def get_choices(self, squares):
+        choices = []
+        for cgroup in self.choices_groups:
+            cdict = OrderedDict()
+            for clabel, clist in sorted(cgroup.items()):
+                cdict[clabel] = list(zip(squares, clist))
+            choices.append(cdict)
+        return choices
+
+
+class Screen1(Page, AbstractScreen):
+
+    template_name = "preference_for_skew/Question.html"
 
     form_model = models.Player
     form_fields = ['skewchoice1']
+    choices_groups = (Constants.skewchoices1,)
 
     def vars_for_template(self):
-        pie, squares = graph.render(**Constants.pie_conf)
+        pie, squares = graph.render(title="Please Choose your most preferred gamble", **Constants.pie_conf)
+        choices = self.get_choices(squares)
         return {
+            "screen_number": 1,
+            "subtitle": "Individuals makes a choice of wich lottery A-F",
             "graph": pie,
-            "choices": Constants.skewchoices1,
-            "squares": squares}
+            "choices": choices}
+
+
+class Screen2(Page, AbstractScreen):
+
+    template_name = "preference_for_skew/Question.html"
+
+    form_model = models.Player
+    form_fields = ['skewchoice2']
+    choices_groups = (Constants.skewchoices1, Constants.skewchoices2)
+
+    def vars_for_template(self):
+        pie, squares = graph.render(
+            title="Below you can see the choice you mae previously, and Gamble Set 2. Please Choose your most preferred gamble",
+            **Constants.pie_conf)
+        choices = self.get_choices(squares)
+        selected = [self.player.skewchoice1]
+        return {
+            "screen_number": 2,
+            "subtitle": "Individual makes another choice of which lottery A-F, with new values. Can choose new lottery or keep original choice",
+            "graph": pie,
+            "choices": choices,
+            "selected": selected}
+
+
+class Screen3(Page, AbstractScreen):
+
+    template_name = "preference_for_skew/Question.html"
+
+    form_model = models.Player
+    form_fields = ['skewchoice3']
+    choices_groups = (Constants.skewchoices1, Constants.skewchoices2, Constants.skewchoices3)
+
+    def vars_for_template(self):
+        pie, squares = graph.render(
+            title="Below you can see the choice you mae previously, and Gamble Set 3. Please Choose your most preferred gamble",
+            **Constants.pie_conf)
+        choices = self.get_choices(squares)
+        selected = [self.player.skewchoice1, self.player.skewchoice2]
+        return {
+            "screen_number": 3,
+            "subtitle": "Individual makes another choice of which lottery A-F, with new values. Can choose new lottery or keep original choice",
+            "graph": pie,
+            "choices": choices,
+            "selected": selected}
 
 
 page_sequence = [
-    Screen1,
+    Screen1, Screen2, Screen3
     #~ Results
 ]
